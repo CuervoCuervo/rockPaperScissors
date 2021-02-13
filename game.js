eadecf4ef5a3efabba035b8657fd27a3c71ade27
Alejandro <alejandro.leitner@gmail.com>
2021-02-13 15:33:53 -0300

    hacer boton html y PENSAR seleccion y string

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
 let rock = 1
 let scissors = 2
 let paper = 3
-function computerPlay(min, max)
-    Math.random(1, 3);
-function playRound()
-    if (computerPlay() === 1);{
+
+function computerPlay(min, max){
+    Math.floor(Math.random(1, 3));
+}
+
+function playRound(){
+    prompt("Desafio: piedra, papel o tijeras", "ingrese su elección aquí")
+    if (computerPlay() === 1){
         rock;
     }
         if (computerPlay() === 2){
             scissors;
-    }
+        }
         else { 
             paper;
-    }
+        }
+            }
