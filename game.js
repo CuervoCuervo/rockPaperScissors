1bcecec26967684ec866258a2a490965924336ca
Alejandro <alejandro.leitner@gmail.com>
2021-02-13 15:05:45 -0300

    primer drafteo, falta mucho

---
+++ game.js
@@ -0,0 +1,15 @@
+let rock = 1
+let scissors = 2
+let paper = 3
+function computerPlay(min, max)
+    Math.random(1, 3);
+function playRound()
+    if (computerPlay() === 1);{
+        rock;
+    }
+        if (computerPlay() === 2){
+            scissors;
+    }
+        else { 
+            paper;
+    }
