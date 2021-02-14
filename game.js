d0df5d6f5626f5b4eac1aa545024b8d7c83cdd0f
Alejandro <alejandro.leitner@gmail.com>
2021-02-14 06:49:35 -0300

    perdi datos anteriores. rehice secuencia ganar perder empatar. falta html

--- game.js
+++ game.js
@@ -1,13 +1,45 @@
-let rock = 1
-let scissors = 2
-let paper = 3
+const rock = 1
+const scissors = 2
+const paper = 3
+
+function gameStart()
+    playRound();
+    if rock > scissors{
+        "Ganaste"
+    }
+        else if rock < paper{
+            "Perdiste"
+        }
+        else if rock === rock{
+            "Empate"
+        }
+    if paper > rock{
+        "Ganaste"
+    }
+        else if paper < scissors{
+            "Perdiste"
+        }
+        else if paper === scissors{
+            "Empate"
+        }
+    if scissors > paper{
+        "Ganaste"
+    }
+        else if scissors < rock{
+            "Perdiste"
+        }
+        else if scissors === scissors{
+            "Empate"
+        }
+    }
 
 function computerPlay(min, max){
     Math.floor(Math.random(1, 3));
 }
 
+function playerSelection()
+
 function playRound(){
-    prompt("Desafio: piedra, papel o tijeras", "ingrese su elección aquí")
     if (computerPlay() === 1){
         rock;
     }
